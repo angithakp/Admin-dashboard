32101b646e9527f8f1013df88218cc2f61e0f763
angithakp <angithavalsan@gmail.com>
2023-08-14 11:57:16 +0530

    Admin-dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:DashbordComponent},
+  {path:'',redirectTo:'dashbord',pathMatch:'full'},
+  {path:'employee',component:DashbordComponent},
+  {path:'gallery',component:DashbordComponent},
+  {path:'contact',component:DashbordComponent},
+  {path:'leads',component:DashbordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
